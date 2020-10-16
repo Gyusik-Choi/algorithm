8da44264c3de6b649a11b2a97f6d222ab39330f6
kuidoli <kuidoli@hanmail.net>
2020-10-17 00:06:57 +0900

    baekjoon 11654

---
+++ Baekjoon/11654_아스키 코드/B_11654 README.md
@@ -0,0 +1,32 @@
+# 백준
+
+## 11654
+
+아스키코드 -> 10진수 변환
+
+```java
+char ascii = 'A';
+int change = ascii;
+System.out.println(a);
+```
+
+```java
+String ascii = 'A';
+int change = ascii.charAt(0);
+System.out.println(change);
+```
+
+
+
+10진수 -> 아스키코드 변환
+
+```java
+int num = 65;
+System.out.println((char)n);
+```
+
+
+
+참조
+
+https://jiwontip.tistory.com/54
---
+++ Baekjoon/11654_아스키 코드/B_11654.java
@@ -0,0 +1,17 @@
+package baekjoon_java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class B_11654 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String ascii = br.readLine();
+		int change = ascii.charAt(0);
+		System.out.println(change);
+		
+	}
+}
