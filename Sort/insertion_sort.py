24bb1c012671c43aeed75caccc8d5cc3e47d6da0
gyusik <kuidoli@hanmail.net>
2020-12-29 12:14:29 +0900

    insertion sort

--- Sort/insertion_sort.py
+++ Sort/insertion_sort.py
@@ -1,9 +1,17 @@
 arr = [2, 3, 4, 1, 1, 5, 5, 3, 4, 6, 8, 9, 7]
 
-for i in range(len(arr)):
-    min_idx = i
-    for j in range(i + 1, len(arr)):
-        if arr[min_idx] > arr[j]:
-            min_idx = j
-    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+# 삽입 정렬
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j - 1] > arr[j]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+print(arr)
+
+# 삽입 정렬 개선
+for i in range(1, len(arr)):
+    for j in range(i, 0, -1):
+        if arr[j - 1] < arr[j]:
+            break
+        else:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
 print(arr)
