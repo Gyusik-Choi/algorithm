34fd1493d7f2b234e6ffe17e1a8c91f6179a0ad1
gyusik <kuidoli@hanmail.net>
2021-01-04 14:12:25 +0900

    insertion sort

--- Algorithm/Sort/insertion_sort.py
+++ Algorithm/Sort/insertion_sort.py
@@ -15,3 +15,13 @@ for i in range(1, len(arr)):
         else:
             arr[j - 1], arr[j] = arr[j], arr[j - 1]
 print(arr)
+
+# 삽입 정렬 개선 2
+for idx in range(1, len(arr)):
+    i = idx
+    to_insert = arr[idx]
+    while i > 0 and arr[i - 1] > to_insert:
+        arr[i] = arr[i - 1]
+        i -= 1
+    arr[i] = to_insert
+print(arr)
