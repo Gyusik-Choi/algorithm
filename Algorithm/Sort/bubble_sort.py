6f88a83ca536bdfbacbf9a45391d3f732929075a
gyusik <kuidoli@hanmail.net>
2021-01-28 19:40:22 +0900

    update bubble sort

--- Algorithm/Sort/bubble_sort.py
+++ Algorithm/Sort/bubble_sort.py
@@ -1,9 +1,36 @@
-arr = [2, 3, 4, 1, 1, 5, 5, 3, 4, 6, 8, 9, 7]
+# 거품정렬
+arr = [5, 2, 1, 3, 4]
+N = len(arr) - 1
+for i in range(N - 1, -1, -1):
+    for j in range(i + 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        else:
+            break
+print(arr)
 
-# 버블 정렬
-arr_b = copy.deepcopy(arr)
-for i in range(len(arr_b) - 1):
-    for j in range(i+1, len(arr_b)):
-        if arr_b[i] > arr_b[j]:
-            arr_b[i], arr_b[j] = arr_b[j], arr_b[i]
-print(arr_b)
+# 거품정렬 개선
+arr = [5, 2, 1, 3, 4]
+N = len(arr) - 1
+for i in range(N - 1, -1, -1):
+    swapped = False
+    for j in range(i + 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            swapped = True
+    if not swapped:
+        break
+print(arr)
+
+# 거품정렬 추가 개선
+arr = [5, 2, 1, 3, 4]
+idx = len(arr) - 1
+while idx > 0:
+    for i in range(idx):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            idx = i
+print(arr)
+
+# 참고
+# https://www.daleseo.com/sort-bubble/
