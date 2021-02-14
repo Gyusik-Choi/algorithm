3f3e2809d270fe82af8db6f268c3c8941591620e
gyusik <kuidoli@hanmail.net>
2021-02-15 00:08:12 +0900

    circular linked list

--- DataStructure/LinkedList/Circular LinkedList(No Tail)(In Progress).py
+++ DataStructure/LinkedList/Circular LinkedList(No Tail)(In Progress).py
@@ -30,6 +30,7 @@ class CircularLinkedList:
     def append_first(self, item):
         if self.is_empty():
             self.head = Node(item)
+            self.head.next = self.head
         else:
             cur = self.head
             self.head = Node(item)
@@ -71,8 +72,20 @@ class CircularLinkedList:
             return
 
         if idx == 0:
-            pass
-        
+            cur = self.head
+            self.head = self.head.next
+
+            cur2 = self.head
+            while cur2.next is not cur:
+                cur2 = cur2.next
+            cur2.next = self.head
+        else:
+            cur = self.head
+            i = 1
+            while idx > i:
+                cur = cur.next
+                i += 1
+            cur.next = cur.next.next
         self.size -= 1
         pass
 
@@ -86,9 +99,8 @@ class CircularLinkedList:
 cll = CircularLinkedList()
 cll.append(3)
 cll.append_first(2)
-cll.insert(1, 0)
-cll.insert(4, 3)
-cll.append(5)
+cll.append(1)
+cll.remove_by_index(2)
 
 # 참고
 # https://comdoc.tistory.com/entry/%EC%9B%90%ED%98%95-%EC%97%B0%EA%B2%B0-%EB%A6%AC%EC%8A%A4%ED%8A%B8Circular-linked-list-ADT-%ED%8C%8C%EC%9D%B4%EC%8D%AC
