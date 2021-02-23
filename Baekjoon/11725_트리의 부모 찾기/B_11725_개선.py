147d8f5637e27c5628aa21cef4dc98b4fb5e7c44
gyusik <kuidoli@hanmail.net>
2021-02-23 15:58:26 +0900

    baekjoon 11725

---
+++ Baekjoon/11725_트리의 부모 찾기/B_11725_개선.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def dfs(vertex):
+    stack = [vertex]
+    while stack:
+        tmp = stack.pop()
+        for n in node[tmp]:
+            if visited[n] == 0:
+                stack.append(n)
+                p[n] = tmp
+        visited[tmp] = 1
+
+
+N = int(input())
+node = {i: [] for i in range(N + 1)}
+for i in range(N - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    node[a].append(b)
+    node[b].append(a)
+
+p = [0] * (N + 1)
+visited = [0] * (N + 1)
+dfs(1)
+
+for i in range(2, N + 1):
+    sys.stdout.write(str(p[i]) + '\n')
