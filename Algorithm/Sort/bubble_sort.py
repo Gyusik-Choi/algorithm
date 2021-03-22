90e506e945ad0135fb33be2e5e199cafdd9953a6
Gyusik-Choi <57178206+Gyusik-Choi@users.noreply.github.com>
2021-03-22 23:58:54 +0900

    Update bubble_sort.py

--- Algorithm/Sort/bubble_sort.py
+++ Algorithm/Sort/bubble_sort.py
@@ -1,20 +1,18 @@
 # 거품정렬
 arr = [5, 2, 1, 3, 4]
-N = len(arr) - 1
-for i in range(N - 1, -1, -1):
-    for j in range(i + 1):
+N = len(arr)
+for i in range(N - 1, 0, -1):
+    for j in range(i):
         if arr[j] > arr[j + 1]:
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        else:
-            break
 print(arr)
 
 # 거품정렬 개선
 arr = [5, 2, 1, 3, 4]
-N = len(arr) - 1
-for i in range(N - 1, -1, -1):
+N = len(arr)
+for i in range(N - 1, 0, -1):
     swapped = False
-    for j in range(i + 1):
+    for j in range(i):
         if arr[j] > arr[j + 1]:
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
             swapped = True
