d5a9f6a2f47e1a4635e0bddc8e9fb5b445ceef2f
Gyusik <kuidoli@hanmail.net>
2021-05-06 22:50:12 +0900

    baekjoon 11053

---
+++ Baekjoon/11053_가장 긴 증가하는 부분수열 길이/B_11053_3.py
@@ -0,0 +1,27 @@
+def binary_search(start, end, target):
+    while start < end:
+        mid = (start + end) // 2
+
+        if lis[mid] < target:
+            start = mid + 1
+        else:
+            end = mid
+    return end
+
+
+N = int(input())
+numbers = list(map(int, input().split()))
+
+lis = [numbers[0]]
+for i in range(1, len(numbers)):
+    if numbers[i] > lis[-1]:
+        lis.append(numbers[i])
+    else:
+        idx = binary_search(0, len(lis) - 1, numbers[i])
+        lis[idx] = numbers[i]
+
+print(len(lis))
+
+# 참고
+# https://chanhuiseok.github.io/posts/algo-49/
+# https://shoark7.github.io/programming/algorithm/3-LIS-algorithms
