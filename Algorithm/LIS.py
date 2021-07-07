175285db7ab832f8be826cc736f0878902308809
Gyusik <kuidoli@hanmail.net>
2021-07-07 23:46:01 +0900

    LIS

--- Algorithm/LIS.py
+++ Algorithm/LIS.py
@@ -1,6 +1,6 @@
 # LIS (최장 증가 부분 수열) 의 갯수를 구하는 방법
 # 방법1: 완전탐색
-numbers = [10, 20, 30, 10, 30, 40, 20, 50, 60]
+numbers = [10, 20, 30, 10, 30, 40, 20, 50, 60, 20]
 lis = [1] * len(numbers)
 
 for i in range(1, len(numbers)):
@@ -8,4 +8,4 @@ for i in range(1, len(numbers)):
         if numbers[i] > numbers[j]:
             lis[i] = max(lis[j] + 1, lis[i])
 
-print(lis[-1])
+print(max(lis))
