2c104cb32e15553834d90951463f6ca6f326f71b
Gyusik-Choi <57178206+Gyusik-Choi@users.noreply.github.com>
2021-07-14 23:55:43 +0900

    Update B_9375.py

--- Baekjoon/9375_패션왕 신혜빈(진행중)/B_9375.py
+++ Baekjoon/9375_패션왕 신혜빈(진행중)/B_9375.py
@@ -37,5 +37,4 @@ for _ in range(T):
             for i in range(category_cnt):
                 answer *= per_category_cnt[i] + 1
             answer -= 1
-
         sys.stdout.write(str(answer) + "\n")
