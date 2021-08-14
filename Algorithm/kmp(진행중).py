d18f10ae59d61fdd592ceb22bee294649bfcd2fc
kuidoli <kuidoli@hanmail.net>
2021-08-14 15:30:27 +0900

    algorithm kmp

--- Algorithm/kmp(진행중).py
+++ Algorithm/kmp(진행중).py
@@ -1,13 +1,11 @@
-# 문자열 탐색 알고리즘
-# 접두사, 경계, 접미사
-# 탐색을 하기 전에 패턴의 길이별 최대 경계 길이를 구하는 테이블을 만들어둔다
-
-
+# 한가지 간과한 점은
+# 이동거리 테이블을 만들때 문자열의 가운데를 경계로 생각하면 안됐다는 것이다
+# 계속 일지하면 가운데를 넘어서 접두사와 접미사가 경계점을 서로 넘어설 수 있는데 그 점을 제대로 생각하지 못했다
 def kmp():
     answer = -1
     s_length = len(string)
     p_length = len(pattern)
-    distance, idx, cnt = 0, 0, 0
+    idx, distance, cnt = 0, 0, 0
     flag = False
 
     while True:
@@ -18,7 +16,6 @@ def kmp():
         while string[idx + distance] == pattern[cnt]:
             cnt += 1
             idx += 1
-
             if cnt == p_length:
                 answer = distance + 1
                 flag = True
@@ -32,48 +29,32 @@ def kmp():
     return answer
 
 
-# 인덱스별로 따로 구하지 않고 다음 인덱스 값을 구할때 이전 인덱스값을 활용한다
 def create_distance_table(p, p_length):
     table = [0] * (p_length + 1)
     table[0] = -1
-    j = 0
+    length = 0
     for k in range(1, p_length):
-        while p[j] != p[k] and j > 0:
-            j = table[j]
+        while p[length] != p[k] and length > 0:
+            length = table[length]
 
-        if p[j] == p[k]:
-            j += 1
-            table[k + 1] = j
+        if p[length] == p[k]:
+            length += 1
+            table[k + 1] = length
     return table
-
-    # table = [0] * (p_length + 1)
-    # table[0] = -1
-    # idx = 2
-    # length = 0
-    # # 마지막 인덱스는 0으로 두기 위해 while 문의 범위에 포함되지 않도록 조정
-    # while idx < p_length:
-    #     if p[idx - 1] == p[length]:
-    #         idx += 1
-    #         length += 1
-    #         table[idx - 1] = length
-    #     else:
-    #         if length == 0:
-    #             table[idx] = 0
-    #             idx += 1
-    #         else:
-    #             # length -= 1 <- 이건 왜 안되는지 이해 필요
-    #             length = table[length - 1]
-    # return table
 
 
 string = "ABAABACABAACCABACABACABAACABACABAAC"
 pattern = "ABACABAAC"
+# pattern = "ABABABAC"
 
+# 불일치시 이전의 길이의 경계 값을 찾는다.
+# 몇칸을 뛰어넘고 다시 경계 값을 찾아야할지 알기 위해서.
+# https://bowbowbow.tistory.com/6
+# 이 글의 pi 배열 구하는 과정이 이해하는데 많은 도움이 됐다
+
+# ABABAB
+# ABABAC
 distance_table = create_distance_table(pattern, len(pattern))
-
+print(distance_table)
 number = kmp()
 print(number)
-
-# 참고
-# https://chanhuiseok.github.io/posts/algo-14/
-# https://devbull.xyz/python-kmp-algorijeumeuro-munjayeol-cajgi/
