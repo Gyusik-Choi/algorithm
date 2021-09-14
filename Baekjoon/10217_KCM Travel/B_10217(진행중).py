31f6153d495b9b0cc86daf357f20548f03e63479
kuidoli <kuidoli@hanmail.net>
2021-09-14 23:20:26 +0900

    baekjoon 10217

--- Baekjoon/10217_KCM Travel/B_10217(진행중).py
+++ Baekjoon/10217_KCM Travel/B_10217(진행중).py
@@ -1,4 +1,3 @@
-import heapq
 import sys
 
 
@@ -12,8 +11,26 @@ for _ in range(T):
         routes[u].append([v, c, d])
 
     INF = float('inf')
-    dp = [[INF] * (N + 1) for _ in range(N + 1)]
+    dp = [[INF] * (M + 1) for _ in range(N + 1)]
+    dp[1][0] = 0
 
-    heap = []
-    heapq.heappush(heap, (1, 0))
+    for cost in range(M + 1):
+        for node in range(1, N + 1):
+            if dp[node][cost] != INF:
+                for end, value, time in routes[node]:
+                    if cost + value <= M:
+                        dp[end][cost + value] = min(dp[end][cost + value], dp[node][cost] + time)
 
+    if min(dp[N]) != INF:
+        sys.stdout.write(str(min(dp[N])) + "\n")
+    else:
+        sys.stdout.write("Poor KCM" + "\n")
+
+
+# 참고
+# https://maivve.tistory.com/226
+# https://velog.io/@jini_eun/%EB%B0%B1%EC%A4%80-10217%EB%B2%88-KCM-Travel-Java-Python
+# https://developmentdiary.tistory.com/401
+# https://kibbomi.tistory.com/178
+# https://kyunstudio.tistory.com/160
+# https://roamingman.tistory.com/m/46
