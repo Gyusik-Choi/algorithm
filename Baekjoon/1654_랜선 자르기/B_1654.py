2f31ab104e5eaed486c09729d28a5f75a978aa9c
kuidoli <kuidoli@hanmail.net>
2021-09-24 00:33:55 +0900

    baekjoon 1654

--- Baekjoon/1654_랜선 자르기/B_1654.py
+++ Baekjoon/1654_랜선 자르기/B_1654.py
@@ -6,7 +6,7 @@ def binary_search(start, end, target):
         return
 
     mid = (start + end) // 2
-    
+
     sums = 0
     for j in range(K):
         candidate = lan_lines[j]
@@ -14,9 +14,9 @@ def binary_search(start, end, target):
 
     if sums >= target:
         answer_candidates.append(mid)
-        binary_search(mid + 1, end, target)
+        return binary_search(mid + 1, end, target)
     else:
-        binary_search(start, mid - 1, target)
+        return binary_search(start, mid - 1, target)
 
 
 K, N = map(int, sys.stdin.readline().split())
@@ -25,60 +25,15 @@ for i in range(K):
     lan = int(sys.stdin.readline().rstrip())
     lan_lines.append(lan)
 
-if K == N:
-    sys.stdout.write(str(min(lan_lines)))
-else:
-    answer_candidates = []
-    binary_search(1, max(lan_lines), N)
-    sys.stdout.write(str(max(answer_candidates)))
+# if K == N:
+#     sys.stdout.write(str(min(lan_lines)))
+# else:
+answer_candidates = []
+binary_search(1, max(lan_lines), N)
+sys.stdout.write(str(max(answer_candidates)))
 
-# 457, 539, 743, 802
 # K 개의 랜선 각각으로 모두 만들 수 있는 공통적인 최대 길이의 랜선
 # => 아니다. 공통으로 만족하지 않아도 된다. 최대 길이로 N 갯수만 맞출 수 있으면 된다
-
-# 200
-# 2
-# 2
-# 3
-# 4
-
-# 이분 탐색 문제라 선형으로 하나식 찾지 말라는 문제인데...
-
-# 200 300 400 500
-# 12
-
-# 100
-# 2
-# 3
-# 4
-# 5
-
-# 150
-# 1
-# 2
-# 2
-# 3
-
-# 125
-# 1
-# 2
-# 3
-# 4
-
-# 112
-# 1
-# 2
-# 3
-# 4
-
-# (1, 200)
-# 100
-# (101, 200)
-# 150
-# (101, 149)
-# 125
-# (101, 124)
-# 112
 
 # 반례
 # https://joey09.tistory.com/115
@@ -88,5 +43,19 @@ else:
 # 5
 # 5
 # 4
-# 정답 2
+# 정답: 2
 
+# 추가 반례
+# 11 46
+# 2147483635
+# 2147483595
+# 2147483585
+# 2147483641
+# 2147483552
+# 2147483637
+# 2147483570
+# 2147483563
+# 2147483621
+# 2147483618
+# 2147483553
+# 정답: 429496727
