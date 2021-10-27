cec33e73a311729483fa42a5254db3bd91309fb5
kuidoli <kuidoli@hanmail.net>
2021-10-28 00:15:00 +0900

    baekjoon 2178

--- Baekjoon/2178_미로 탐색/B_2178.js
+++ Baekjoon/2178_미로 탐색/B_2178.js
@@ -1,3 +1,16 @@
+const isInQueue = function(q, candidate) {
+    const [yCandidate, xCandidate, cntCandidate] = candidate
+    for (let k = 0; k < q.length; k++) {
+        const [yQ, xQ, cntQ] = q[k]
+
+        if (yCandidate === yQ && xCandidate === xQ && cntCandidate === cntQ) {
+            return true
+        }
+    }
+
+    return false
+}
+
 const bfs = function(y, x) {
     let visited = new Array(N).fill(0).map(v => new Array(M).fill(0))
     let queue = [[y, x, 1]]
@@ -13,12 +26,14 @@ const bfs = function(y, x) {
             const yWay = yAxis + yDirection[j]
             const xWay = xAxis + xDirection[j]
 
-            if (yWay === N - 1 && xWay === M - 1) {
-                return cnt + 1
-            }
+            if (0 <= yWay && yWay < N && 0 <= xWay && xWay < M) {
+                if (yWay === N - 1 && xWay === M - 1) {
+                    return cnt + 1
+                }
 
-            if (maze[yWay][xWay] === 1 && visited[yWay][xWay] === 0) {
-                queue.push([yWay, xWay, cnt + 1])
+                if (maze[yWay][xWay] === 1 && visited[yWay][xWay] === 0 && isInQueue(queue, [yWay, xWay, cnt + 1]) === false) {
+                    queue.push([yWay, xWay, cnt + 1])
+                }
             }
         }
     }
@@ -30,10 +45,10 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [N, M] = input[0].split(' ').map(v => Number(v))
 let maze = []
-for (let i = 0; i < N; i++) {
+for (let i = 1; i <= N; i++) {
     const coordinate = input[i].split('').map(v => Number(v))
     maze.push(coordinate)  
 }
 
-const answer = bfs(N - 1, M - 1)
+const answer = bfs(0, 0)
 console.log(answer)
