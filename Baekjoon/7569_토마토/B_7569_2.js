5598e4d86434516e275eb74fd44fbe14b9aae095
kuidoli <kuidoli@hanmail.net>
2022-01-31 23:13:33 +0900

    baekjoon 7569

--- Baekjoon/7569_토마토/B_7569_2.js
+++ Baekjoon/7569_토마토/B_7569_2.js
@@ -29,7 +29,7 @@ Queue.prototype.enQueue = function(item) {
 
 Queue.prototype.deQueue = function() {
     if (this.isEmpty() === false) {
-        const popNumber = this.tail.value
+        const popNumber = this.head.value
 
         if (this.head === this.tail) {
             this.head = new Node(null)
@@ -42,10 +42,10 @@ Queue.prototype.deQueue = function() {
     }
 }
 
-const isAllRipen = function(h, n, m) {
-    for (let i = 0; i < h; i++) {
-        for (let j = 0; j < n; j++) {
-            for (let k = 0; k < m; k++) {
+const isAllRipen = function() {
+    for (let i = 0; i < H; i++) {
+        for (let j = 0; j < N; j++) {
+            for (let k = 0; k < M; k++) {
                 if (boxes[i][j][k] === 0) {
                     return false
                 }
@@ -65,18 +65,7 @@ const isValidRange = function(h, n, m) {
 }
 
 const bfs = function(q) {
-    // visited 에 날짜를 계산해나갈까 고민했지만
-    // visited 는 방문 여부만 체크하고
-    // deq 에 담을때 날짜를 계산해서 담아가는 방식으로 진행한다
-    // => visited 가 필요없다
-
-    let maxDay = 0
-
-    // let visited = new Array(H).fill(false).map(v => 
-    //     new Array(N).fill(false).map(v => 
-    //         new Array(M).fill(false)
-    //     )
-    // )
+    let maxDay = 1
 
     const zDirection = [-1, 1, 0, 0, 0, 0]
     const yDirection = [0, 0, -1, 0, 1, 0]
@@ -84,25 +73,17 @@ const bfs = function(q) {
 
     while (q.isEmpty() === false) {
         const [z, y, x] = q.deQueue()
-        // visited[z][y][x] = true
 
         for (let i = 0; i < 6; i++) {
             const zIdx = z + zDirection[i]
             const yIdx = y + yDirection[i]
             const xIdx = x + xDirection[i]
 
-            if (0 <= zIdx && zIdx < H && 0 <= yIdx && yIdx < N && 0 <= xIdx && xIdx < M) {
-            // if (isValidRange(zIdx, yIdx, xIdx)) {
+            if (isValidRange(zIdx, yIdx, xIdx)) {
                 if (boxes[zIdx][yIdx][xIdx] === 0) {
-                    // if (visited[zIdx][yIdx][xIdx] === false) {
-                    //     boxes[zIdx][yIdx][xIdx] = 1
-                    //     maxDay = Math.max(maxDay, day + 1)
-                    //     q.enQueue([zIdx, yIdx, xIdx, day + 1])
-                    // }
                     boxes[zIdx][yIdx][xIdx] = boxes[z][y][x] + 1
                     maxDay = Math.max(maxDay, boxes[zIdx][yIdx][xIdx])
                     q.enQueue([zIdx, yIdx, xIdx])
-                    
                 }
             }
         }
@@ -111,25 +92,8 @@ const bfs = function(q) {
     return maxDay
 }
 
-const input = [
-'5 3 2',
-'0 0 0 0 0',
-'0 0 0 0 0',
-'0 0 0 0 0',
-'0 0 0 0 0',
-'0 0 1 0 0',
-'0 0 0 0 0'
-]
-
-// const input = [
-// '4 3 2',
-// '1 1 1 1',
-// '1 1 1 1',
-// '1 1 1 1',
-// '1 1 1 1',
-// '-1 -1 -1 -1',
-// '1 1 1 -1'
-// ]
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 
 const [M, N, H] = input[0].split(' ').map(v => parseInt(v))
 let boxes = []
@@ -161,7 +125,7 @@ for (let i = 0; i < H; i++) {
 const answer = bfs(deq)
 
 if (isAllRipen()) {
-    console.log(answer === 0 ? 0 : answer - 1)
+    console.log(answer - 1)
 } else {
     console.log(-1)
 }
