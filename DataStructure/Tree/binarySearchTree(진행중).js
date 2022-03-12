dc2725e6c8ddd9ab439aec5bc5f1dc3d2d81bf34
kuidoli <kuidoli@hanmail.net>
2022-03-12 15:36:28 +0900

    binarysearchtree

---
+++ DataStructure/Tree/binarySearchTree(진행중).js
@@ -0,0 +1,33 @@
+const Node = function(value, left = null, right = null) {
+    this.vaue = value;
+    this.left = left;
+    this.right = right;
+}
+
+const BinarySearchTree = function() {
+    this.head = new Node(null);
+}
+
+BinarySearchTree.prototype.search = function() {
+
+}
+
+BinarySearchTree.prototype.add = function() {
+
+}
+
+BinarySearchTree.prototype.remove = function() {
+
+}
+
+BinarySearchTree.prototype.preOrder = function() {
+
+}
+
+BinarySearchTree.prototype.inOrder = function() {
+    
+}
+
+BinarySearchTree.prototype.postOrder = function() {
+    
+}
