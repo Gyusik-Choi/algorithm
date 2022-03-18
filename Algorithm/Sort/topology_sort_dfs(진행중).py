edf86c9364060d554bf710bd7ac12f004bb738c8
최 규식 <kuidoli@hanmail.net>
2022-03-18 14:42:18 +0900

    topology sort dfs

---
+++ Algorithm/Sort/topology_sort_dfs(진행중).py
@@ -0,0 +1,40 @@
+def dfs(start):
+    for end in edges[start]:
+        if not visited[end]:
+            visited[end] = 1
+            dfs(end)
+    stack.append(start)
+
+
+# 정점, 간선 갯수
+v, e = map(int, input().split())
+edges = {i: [] for i in range(1, v + 1)}
+
+for _ in range(e):
+    s, e = map(int, input().split())
+    edges[s].append(e)
+
+visited = [0] * (v + 1)
+stack = []
+for i in range(1, v + 1):
+    if not visited[i]:
+        visited[i] = 1
+        dfs(i)
+
+for i in range(len(stack) - 1, -1, -1):
+    print(stack[i], end=" ")
+print()
+
+# 7 8
+# 1 2
+# 1 5
+# 2 3
+# 2 6
+# 3 4
+# 4 7
+# 5 6
+# 6 4
+# => 1 5 2 6 3 4 7
+
+# 이것이 코딩 테스드다 (나동빈)
+# https://velog.io/@jeongmin/%EC%9C%84%EC%83%81-%EC%A0%95%EB%A0%ACtopological-sorting
