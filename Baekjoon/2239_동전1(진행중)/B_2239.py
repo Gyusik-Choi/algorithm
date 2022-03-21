49e2c768279fba80025df3152cb904d6ef1b0611
kuidoli <kuidoli@hanmail.net>
2022-03-21 21:25:48 +0900

    baekjoon 2239

--- Baekjoon/2239_동전1(진행중)/B_2239.py
+++ Baekjoon/2239_동전1(진행중)/B_2239.py
@@ -25,15 +25,30 @@ print(dp[k])
 # dp[3] += dp[3 - 1]
 # dp[4] += dp[4 - 1]
 # dp[5] += dp[5 - 1]
+# dp[6] += dp[6 - 1]
+# dp[7] += dp[7 - 1]
+# dp[8] += dp[8 - 1]
+# dp[9] += dp[9 - 1]
+# dp[10] += dp[10 - 1]
 
 # coin = 2
 # dp[2] += dp[2 - 2]
 # dp[3] += dp[3 - 2]
 # dp[4] += dp[4 - 2]
 # dp[5] += dp[5 - 2]
+# dp[6] += dp[6 - 2]
+# dp[7] += dp[7 - 2]
+# dp[8] += dp[8 - 2]
+# dp[9] += dp[9 - 2]
+# dp[10] += dp[10 - 2]
 
 # coin = 5
 # dp[5] += dp[5 - 5]
+# dp[6] += dp[6 - 5]
+# dp[7] += dp[7 - 5]
+# dp[8] += dp[8 - 5]
+# dp[9] += dp[9 - 5]
+# dp[10] += dp[10 - 5]
 
 # https://yabmoons.tistory.com/491
 # https://debuglog.tistory.com/78
