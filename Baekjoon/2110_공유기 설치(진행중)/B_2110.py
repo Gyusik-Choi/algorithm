120c10cbcc3d6f032466e0d999e3adb62dd12cfc
kuidoli <kuidoli@hanmail.net>
2022-05-18 23:17:58 +0900

    baekjoon 2110

--- Baekjoon/2110_공유기 설치(진행중)/B_2110.py
+++ Baekjoon/2110_공유기 설치(진행중)/B_2110.py
@@ -2,34 +2,30 @@ import sys
 
 
 def get_max_distance(limit):
-    start = 0
-    end = 1
-
+    start = houses[0]
     cnt = 1
 
-    while end < N:
-        if houses[end] - houses[start] > limit:
-            start += 1
-            end += 1
+    for i in range(1, N):
+        if houses[i] - start >= limit:
             cnt += 1
-        else:
-            end += 1
+            start = houses[i]
 
     return cnt
 
 
 def binary_search(start, end):
+    global answer
     if start > end:
         return
 
     mid = (start + end) // 2
+    cnt = get_max_distance(mid)
 
-    if get_max_distance(mid) >= C:
+    if cnt >= C:
+        answer = mid
         return binary_search(mid + 1, end)
     else:
-        return binary_search(start, mid)
-
-
+        return binary_search(start, mid - 1)
 
 
 N, C = map(int, sys.stdin.readline().split())
@@ -40,6 +36,9 @@ for _ in range(N):
 
 houses.sort()
 
-binary_search(0, N - 1)
+answer = 1
 
-# https://st-lab.tistory.com/277
+# 최소 거리, 최대 거리
+binary_search(1, houses[-1] - houses[0])
+
+print(answer)
