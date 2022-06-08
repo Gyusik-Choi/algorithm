39c99eaf5272ca455b0abc7c890becc007b12826
kuidoli <kuidoli@hanmail.net>
2022-06-08 23:49:01 +0900

    this is coding test ch16 35

--- ThisIsCodingTest/이것이코딩테스트다_16장_못생긴수(잘못된접근).py
+++ ThisIsCodingTest/이것이코딩테스트다_16장_못생긴수(잘못된접근).py
@@ -1,3 +1,5 @@
+n = int(input())
+
 numbers = [1]
 num2 = 2
 num3 = 3
@@ -12,7 +14,8 @@ while len(numbers) < 1000:
         will_add_nums.append(num5 * num)
 
     will_add_nums = list(set(will_add_nums))
-    numbers += will_add_nums
     last_added_nums = will_add_nums
+    numbers += will_add_nums
 
-print(sorted(numbers))
+sorted_numbers = sorted(numbers)
+print(sorted_numbers[n - 1])
