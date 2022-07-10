af6cf65b07a73bb99b8c977dc0f77cde1730e569
kuidoli <kuidoli@hanmail.net>
2022-07-10 16:07:16 +0900

    baekjoon 1766

--- Baekjoon/1766_문제집(진행중)/B_1766.py
+++ Baekjoon/1766_문제집(진행중)/B_1766.py
@@ -2,23 +2,42 @@
 # 진입 차수를 활용하는 bfs 와 다르다
 
 import sys
+import heapq
 
 
-def dfs_recursion():
-    pass
+def bfs(heap):
+    answer = []
+
+    while heap:
+        question = heapq.heappop(heap)
+        answer.append(str(question))
+
+        for q in questions[question]:
+            level[q] -= 1
+            if level[q] == 0:
+                heapq.heappush(heap, q)
+
+    return answer
 
 
 def topology_sort():
-    pass
+    heap = []
+    for i in range(1, N + 1):
+        if not level[i]:
+            heapq.heappush(heap, i)
+
+    return bfs(heap)
 
 
 N, M = map(int, sys.stdin.readline().split())
 questions = {i: [] for i in range(1, N + 1)}
+level = [0] * (N + 1)
 for _ in range(M):
     A, B = map(int, sys.stdin.readline().split())
     questions[A].append(B)
+    level[B] += 1
 
-print(topology_sort())
+print(' '.join(topology_sort()))
 
 # 4 2
 # 1 4
