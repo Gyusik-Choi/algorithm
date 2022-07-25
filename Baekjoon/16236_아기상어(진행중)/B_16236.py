4eb081062fbc1cf1c5fff73e6f5dfe330d792960
최 규식 <kuidoli@hanmail.net>
2022-07-25 13:33:40 +0900

    baekjoon 16236

--- Baekjoon/16236_아기상어(진행중)/B_16236.py
+++ Baekjoon/16236_아기상어(진행중)/B_16236.py
@@ -1,75 +1,90 @@
-import sys
 from collections import deque
+import heapq
 
 
-def bfs(s_y, s_x):
-    q = deque([(s_y, s_x, 0)])
+def bfs(shark_y_idx, shark_x_idx):
+    deq = deque()
+    deq.append([0, shark_y_idx, shark_x_idx])
 
-    dy = [-1, 0, 1, 0]
-    dx = [0, 1, 0, -1]
+    y_direction = [-1, 0, 1, 0]
+    x_direction = [0, 1, 0, -1]
 
-    dist_list = []
-    visited = [[False] * n for _ in range(n)]
-    visited[s_y][s_x] = True
-    min_dist = int(1e9)
+    min_dist = 401
+    visited = [[False] * N for _ in range(N)]
+    visited[shark_y_idx][shark_x_idx] = True
+    heap = []
 
-    while q:
-        y, x, dist = q.popleft()
+    while deq:
+        dist, y_idx, x_idx = deq.popleft()
+
+        if dist > min_dist:
+            break
+
         for k in range(4):
-            y_idx = dy[k] + y
-            x_idx = dx[k] + x
-            if 0 <= y_idx < n and 0 <= x_idx < n and not visited[y_idx][x_idx]:
-                if board[y_idx][x_idx] <= shark_size:
-                    visited[y_idx][x_idx] = True
-                    if 0 < board[y_idx][x_idx] < shark_size:
-                        min_dist = dist
-                        dist_list.append((dist+1, y_idx, x_idx))
-                    if dist + 1 <= min_dist:
-                        q.append((y_idx, x_idx, dist+1))
+            y = y_direction[k] + y_idx
+            x = x_direction[k] + x_idx
 
-    if dist_list:
-        dist_list.sort()
-        return dist_list[0]
+            if 0 <= y < N and 0 <= x < N:
+                # 방문 안했고 상어 크기보다 크거나 같은 지점
+                # 케이스 3개 => 0, shark_size 보다 작은 지점, shark_size 와 같은 지점
+                if not visited[y][x] and sea[y][x] <= shark_size:
+                    visited[y][x] = True
+
+                    # 먹을 수 있는 물고기가 있으면
+                    # 여기보다 더 멀리있는 물고기를 찾을 필요가 없어서
+                    # 큐에 담지 않는다
+                    if 0 < sea[y][x] < shark_size:
+                        min_dist = dist
+                        heapq.heappush(heap, [dist + 1, y, x])
+                    else:
+                        # 0 혹은 shark_size 와 같은 크기의 물고기면서
+                        # 해당 지점에서 한칸씩 더 가도 최소 거리 보다 작거나 같을 경우
+                        # 추가적으로 탐색할 수 있다
+                        if dist + 1 <= min_dist:
+                            deq.append([dist + 1, y, x])
+
+    if heap:
+        distance, y_axis, x_axis = heapq.heappop(heap)
+        return [distance, y_axis, x_axis]
     else:
         return False
 
 
-n = int(sys.stdin.readline())
-board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-shark_y = 0
-shark_x = 0
+def find_shark():
+    shark_y_idx = -1
+    shark_x_idx = -1
+
+    for i in range(N):
+        for j in range(N):
+            if sea[i][j] == 9:
+                shark_y_idx = i
+                shark_x_idx = j
+                sea[i][j] = 0
+
+    return [shark_y_idx, shark_x_idx]
+
+
+N = int(input())
+sea = [list(map(int, input().split())) for _ in range(N)]
+shark_y, shark_x = find_shark()
 shark_size = 2
-eat_cnt = 0
-fish_cnt = 0
-fish_pos = []
+fish = 0
 time = 0
 
-for i in range(n):
-    for j in range(n):
-        if 0 < board[i][j] <= 6:
-            fish_cnt += 1
-            fish_pos.append((i, j))
-        elif board[i][j] == 9:
-            shark_y, shark_x = i, j
-
-board[shark_y][shark_x] = 0
-
-while fish_cnt:
-    result = bfs(shark_y, shark_x)
-    if not result:
+while True:
+    bfs_result = bfs(shark_y, shark_x)
+    if not bfs_result:
         break
 
-    shark_y, shark_x = result[1], result[2]
-    time += result[0]
-    eat_cnt += 1
-    fish_cnt -= 1
+    min_distance, y_info, x_info = bfs_result
+    time += min_distance
+    shark_y = y_info
+    shark_x = x_info
+    sea[shark_y][shark_x] = 0
 
-    if shark_size == eat_cnt:
+    fish += 1
+    if fish == shark_size:
+        fish = 0
         shark_size += 1
-        eat_cnt = 0
-    board[shark_y][shark_x] = 0
 
 print(time)
-
-# 참고
-# https://11001.tistory.com/96
