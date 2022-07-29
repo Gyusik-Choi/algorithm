19404ee5b2ff19e0c2b097e141d0bd5cc085a3c1
최 규식 <kuidoli@hanmail.net>
2022-07-29 15:45:26 +0900

    baekjoon 19236

--- Baekjoon/19236_청소년 상어(진행중)/B_19236(진행중).py
+++ Baekjoon/19236_청소년 상어(진행중)/B_19236(진행중).py
@@ -13,7 +13,7 @@ def is_fish_possible_to_move(fish_way, y_idx, x_idx):
         y = y_idx + dy[fish_way]
         x = x_idx + dx[fish_way]
 
-        if 0 <= y_idx < 4 and 0 <= x_idx < 4 and sea[y_idx][x_idx] == -1:
+        if 0 <= y < 4 and 0 <= x < 4 and sea[y][x] == -1:
             return [y, x]
         else:
             cnt_num += 1
@@ -53,34 +53,31 @@ def fish_move(visited):
                 switch_fish(fish_y, fish_x, result[0], result[1])
 
 
-def get_possible_shark_move(s_y, s_x, visited):
+def get_possible_shark_move(s_y, s_x, way, possible_list):
+    y = s_y + dy[way]
+    x = s_x + dx[way]
+
+    if 0 <= y < 4 and 0 <= x < 4:
+        possible_list.append([y, x])
+        get_possible_shark_move(y, x, way, possible_list)
+
+    return possible_list
+
+
+def back_tracking_shark(s_y, s_x, visited, fish_cnt):
+    global max_fish_cnt
+    fish_move(visited)
     shark_way = sea_way[s_y][s_x]
     shark_way -= 1
+    possible_shark_move = get_possible_shark_move(s_y, s_x, shark_way, [])
 
-    possible_shark_move = []
-    while True:
-        y = s_y + dy[shark_way]
-        x = s_x + dx[shark_way]
-
-        if 0 <= y < 4 and 0 <= x < 4:
-            possible_shark_move.append([y, x])
-        else:
-            break
-
-    return possible_shark_move
-
-
-def back_tracking_shark(s_y, s_x, visited):
-    possible_shark_move = get_possible_shark_move(s_y, s_x, visited)
-    fish_move(visited)
-
-    visited[s_y][s_x] = 1
-
-    for k in range(8):
-        y = s_y + dy[k]
-        x = s_x + dx[k]
-
-    return False
+    for k in range(len(possible_shark_move)):
+        y, x = possible_shark_move[k]
+        original_visited_value = visited[y][x]
+        visited[y][x] = -1
+        max_fish_cnt = max(max_fish_cnt, fish_cnt + 1)
+        back_tracking_shark(y, x, visited, fish_cnt + 1)
+        visited[y][x] = original_visited_value
 
 
 fish_info = [list(map(int, input().split())) for _ in range(4)]
@@ -113,7 +110,10 @@ sea[0][0] = -1
 answer += sea[0][0]
 
 copied_sea = copy.deepcopy(sea)
-back_tracking_shark(0, 0, copied_sea)
+max_fish_cnt = 1
+back_tracking_shark(0, 0, copied_sea, max_fish_cnt)
+
+print(max_fish_cnt)
 
 # 상어가 이동할 수 있는 칸이 여러개 일때 어디로 갈지 어떻게 결정?
 # => 백트래킹
