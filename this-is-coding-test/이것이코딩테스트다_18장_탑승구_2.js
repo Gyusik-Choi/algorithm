4ca31ab43613d9a1a1d3f1f457c3fa64b7f7017f
최 규식 <kuidoli@hanmail.net>
2023-01-22 23:37:17 +0900

    this is coding test ch18 42

---
+++ this-is-coding-test/이것이코딩테스트다_18장_탑승구_2.js
@@ -0,0 +1,78 @@
+class DisjointSet {
+  constructor(n) {
+    this.parent = new Array(n + 1).fill(0);
+    this.makeSet(n);
+  }
+
+  makeSet(n) {
+    for (let i = 0; i <= n; i++) {
+      this.parent[i] = i;
+    }
+  }
+
+  findSet(x) {
+    if (this.parent[x] !== x) {
+      this.parent[x] = this.findSet(this.parent[x]);
+    }
+
+    return this.parent[x];
+  }
+
+  unionSet(x, y) {
+    const px = this.findSet(x);
+    const py = this.findSet(y);
+
+    this.parent[py] = px;
+  }
+}
+
+// const input = [
+//   '4',
+//   '3',
+//   '4',
+//   '1',
+//   '1',
+// ];
+// => 2
+
+const input = [
+  '4',
+  '6',
+  '2',
+  '2',
+  '3',
+  '3',
+  '4',
+  '4',
+];
+// // => 3
+
+const G = parseInt(input.shift());
+const P = parseInt(input.shift());
+
+const gates = [];
+
+for (let i = 0; i < P; i++) {
+  // 탑승구 번호를 0 ~ G - 1 로 하기 위해
+  gates.push(parseInt(input.shift()));
+}
+
+const disjointSet = new DisjointSet(G);
+
+let answer = 0;
+
+for (const gate of gates) {
+  if (disjointSet.findSet(gate) === 0) {
+    break;
+  }
+
+  disjointSet.unionSet(gate, gate - 1);
+
+  answer += 1;
+}
+
+console.log(answer);
+
+// maksSet 으로 초기화한 DisjointSet 의
+// parent 배열의 값을
+// 어떻게 처리해서 도킹 여부를 판단할지 의문
