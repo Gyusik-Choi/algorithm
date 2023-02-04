ebd8f07fb57d9e405124ee46c6cbf198ffcbe971
kuidoli <kuidoli@hanmail.net>
2023-02-04 13:17:04 +0900

    refactor quick_sort and add unit test

--- algorithm/sort/quick_sort.py
+++ algorithm/sort/quick_sort.py
@@ -1,27 +1,49 @@
-def partition(low, high):
+import unittest
+
+
+def partition(arr, low, high):
     pivot = arr[(low + high) // 2]
+
     while low <= high:
         while arr[low] < pivot:
             low += 1
+
         while arr[high] > pivot:
             high -= 1
+
         if low <= high:
             arr[low], arr[high] = arr[high], arr[low]
             low, high = low + 1, high - 1
+
     return low
 
 
-def quick_sort(low, high):
+def sort(arr, low, high):
     if high <= low:
         return
-    mid = partition(low, high)
-    quick_sort(low, mid - 1)
-    quick_sort(mid, high)
+
+    mid = partition(arr, low, high)
+
+    sort(arr, low, mid - 1)
+    sort(arr, mid, high)
+
+    return arr
 
 
-arr = [5, 1, 4, 3, 2]
-quick_sort(0, len(arr) - 1)
-print(arr)
+def quick_sort(arr):
+    return sort(arr, 0, len(arr) - 1)
+
+
+class QuickSortTest(unittest.TestCase):
+    def test_quick_sort(self):
+        lst = [5, 1, 4, 3, 2]
+        sorted_lst = quick_sort(lst)
+        self.assertEqual(sorted_lst, sorted(lst))
+
+    def test_quick_sort_2(self):
+        lst = [5, 1, 4, 3, 2, 7, 6, 9, 8]
+        sorted_lst = quick_sort(lst)
+        self.assertEqual(sorted_lst, sorted(lst))
 
 # 참고
 # https://www.daleseo.com/sort-quick/
