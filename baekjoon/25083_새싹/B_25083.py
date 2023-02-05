83e08e3fa1ac64458bfbd0e4f1ff63048e0f2947
kuidoli <kuidoli@hanmail.net>
2023-02-05 23:43:51 +0900

    baekjoon 25083

---
+++ baekjoon/25083_새싹/B_25083.py
@@ -0,0 +1,10 @@
+import sys
+
+
+first = '         ,r\'\"7'
+second = 'r`-_   ,\'  ,/'
+third = ' \. \". L_r\''
+fourth = '   `~\/'
+fifth = '      |'
+
+sys.stdout.write(first + "\n" + second + "\n" + third + "\n" + fourth + "\n" + fifth + "\n" + fifth)
