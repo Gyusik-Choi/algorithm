56508b6558eb25bc9326b7b17d68f2153b3a88e1
kuidoli <kuidoli@hanmail.net>
2023-04-02 23:00:15 +0900

    baekjoon 9086

---
+++ baekjoon/9086_문자열/B_9086.py
@@ -0,0 +1,8 @@
+T = int(input())
+for _ in range(T):
+    string = input()
+    print(f'{string[0]}{string[-1]}')
+
+# 참고
+# https://docs.python.org/ko/3/tutorial/inputoutput.html
+# https://docs.python.org/ko/3/reference/lexical_analysis.html#f-strings
---
+++ baekjoon/9086_문자열/README.md
@@ -0,0 +1,12 @@
+# 백준
+
+## 9086
+
+파이썬 3.6부터 지원되는 f-string 방식으로 입력 받은 문자열의 처음과 끝을 출력했다.
+
+<br>
+
+<참고>
+
+https://docs.python.org/ko/3/reference/lexical_analysis.html#f-strings
+
