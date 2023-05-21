45591641153f0a214864432a283ff3e7efb837fb
kuidoli <kuidoli@hanmail.net>
2023-05-21 23:27:21 +0900

    add unit test of selection sort

--- algorithm/sort/selection_sort.py
+++ algorithm/sort/selection_sort.py
@@ -1,9 +1,19 @@
-arr = [2, 3, 4, 1, 1, 5, 5, 3, 4, 6, 8, 9, 7]
+from unittest import TestCase
 
-for i in range(len(arr)):
-    min_idx = i
-    for j in range(i + 1, len(arr)):
-        if arr[min_idx] > arr[j]:
-            min_idx = j
-    arr[i], arr[min_idx] = arr[min_idx], arr[i]
-print(arr)
+
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
+
+
+class SelectionSortTest(TestCase):
+    def test_selection_sort(self):
+        arr = [2, 3, 4, 1, 1, 5, 5, 3, 4, 6, 8, 9, 7]
+        sorted_arr = selection_sort(arr)
+        self.assertEqual(sorted(arr), sorted_arr)
