e9a18fa454bf9c5ebaef6d9b31ce93e06f6de183
kuidoli <kuidoli@hanmail.net>
2023-05-22 23:26:55 +0900

    Refactor: change global code as function and add unit test

--- algorithm/sort/counting_sort.py
+++ algorithm/sort/counting_sort.py
@@ -1,16 +1,29 @@
-arr = [2, 3, 4, 1, 1, 5, 5, 3, 4, 6, 8, 9, 7]
+from unittest import TestCase
 
-max_num = max(arr)
-max_arr = [0] * (max_num + 1)
-new_arr = [0] * len(arr)
-for i in range(len(arr)):
-    max_arr[arr[i]] += 1
-for i in range(1, len(max_arr)):
-    max_arr[i] += max_arr[i - 1]
-for i in range(len(new_arr)):
-    max_arr[arr[i]] -= 1
-    new_arr[max_arr[arr[i]]] = arr[i]
-print(new_arr)
+
+def counting_sort(arr):
+    max_num = max(arr)
+    max_arr = [0] * (max_num + 1)
+    new_arr = [0] * len(arr)
+
+    for i in range(len(arr)):
+        max_arr[arr[i]] += 1
+
+    for i in range(1, len(max_arr)):
+        max_arr[i] += max_arr[i - 1]
+
+    for i in range(len(new_arr)):
+        max_arr[arr[i]] -= 1
+        new_arr[max_arr[arr[i]]] = arr[i]
+
+    return new_arr
+
+
+class CountingSortTest(TestCase):
+    def test_counting_sort(self):
+        arr = [2, 3, 4, 1, 1, 5, 5, 3, 4, 6, 8, 9, 7]
+        self.assertEqual(counting_sort(arr), sorted(arr))
+
 
 # 백준 10989 문제 참고
 # 이 문제에서 주의할 점은 메모리 제한이 있기에 배열의 크기를 잡을 때 입력 값의 갯수가 아니라 최대 값을 기준으로 삼아야 한다.
