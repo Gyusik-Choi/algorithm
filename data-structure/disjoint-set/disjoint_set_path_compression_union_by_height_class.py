370529a2416a05530453d719544ae22c6088ce20
최 규식 <kuidoli@hanmail.net>
2023-06-01 19:32:35 +0900

    Refactor: disjoint set

--- data-structure/disjoint-set/disjoint_set_path_compression_union_by_height_class.py
+++ data-structure/disjoint-set/disjoint_set_path_compression_union_by_height_class.py
@@ -1,8 +1,8 @@
 class DisjointSet:
     def __init__(self, n):
-        self.size = n
         self.p = [0] * (n + 1)
         self.rank = [0] * (n + 1)
+
         for i in range(1, n + 1):
             self.make_set(i)
 
@@ -10,17 +10,21 @@ class DisjointSet:
         self.p[x] = x
 
     def find_set(self, x):
-        if self.p[x] == x:
-            return x
-        self.p[x] = self.find_set(self.p[x])
+        if self.p[x] != x:
+            self.p[x] = self.find_set(self.p[x])
         return self.p[x]
 
     def union(self, x, y):
         px = self.find_set(x)
         py = self.find_set(y)
 
+        # px 의 rank 가 크거나 같은 경우
+        # py 를 px 에 합친다
         if self.rank[px] >= self.rank[py]:
             self.p[py] = px
+
+            # px 와 py 의 rank 가 같은 경우
+            # py 의 rank 를 1 증가 시킨다
             if self.rank[px] == self.rank[py]:
                 self.rank[py] += 1
         else:
@@ -44,3 +48,6 @@ disjoint.union(1, 5)
 disjoint.union(6, 7)
 print(disjoint)
 print(disjoint.rank)
+
+# 참고
+# https://ratsgo.github.io/data%20structure&algorithm/2017/11/12/disjointset/
