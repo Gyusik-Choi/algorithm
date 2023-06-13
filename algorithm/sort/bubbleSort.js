ea12d08eb6dd4587a31b713d37e5f1ce0944fe01
kuidoli <kuidoli@hanmail.net>
2023-06-13 23:31:40 +0900

    Refactor: change global to function

--- algorithm/sort/bubbleSort.js
+++ algorithm/sort/bubbleSort.js
@@ -1,13 +1,19 @@
-let arr = [2, 1, 5, 3, 4]
+const bubbleSort = (lst) => {
+  const arr = [...lst];
 
-for (let i = arr.length - 1; i > -1; i--) {
+  for (let i = arr.length - 1; i > -1; i--) {
     for (let j = 0; j <= i; j++) {
-        if (arr[j] > arr[j + 1]) {
-            const temp = arr[j]
-            arr[j] = arr[j + 1]
-            arr[j + 1] = temp
-        }
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
     }
+  }
+
+  return arr;
 }
 
-console.log(arr)
+const arr = [2, 1, 5, 4, 3];
+const sortedArr = bubbleSort(arr);
+console.log(sortedArr);
