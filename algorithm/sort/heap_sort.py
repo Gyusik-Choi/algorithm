7929ee49917cd05d6d84d19e7e3aba4359fce8c4
kuidoli <kuidoli@hanmail.net>
2023-06-18 23:48:43 +0900

    Refactor: change method name and add unit test

--- algorithm/sort/heap_sort.py
+++ algorithm/sort/heap_sort.py
@@ -1,9 +1,12 @@
+from unittest import TestCase
+
+
 class MinHeap:
     def __init__(self):
-        self.arr = [None]
+        self.arr: list = [None]
         self.size = 1
 
-    def insert(self, item):
+    def heap_push(self, item):
         self.arr.append(item)
         self.size += 1
         idx = self.size - 1
@@ -14,7 +17,7 @@ class MinHeap:
             else:
                 break
 
-    def remove(self):
+    def heap_pop(self) -> int:
         self.arr[1], self.arr[-1] = self.arr[-1], self.arr[1]
         min_num = self.arr.pop()
         self.size -= 1
@@ -34,17 +37,20 @@ class MinHeap:
 
         if parent != idx:
             self.arr[parent], self.arr[idx] = self.arr[idx], self.arr[parent]
-            self.min_heapify(parent)
+            return self.min_heapify(parent)
 
 
-mh = MinHeap()
-sorted_arr = []
-arr = [2, 1, 5, 3, 4, 7, 6]
-for i, n in enumerate(arr):
-    mh.insert(n)
+class HeapSortTest(TestCase):
+    def test_heap_sort(self):
+        mh = MinHeap()
+        sorted_arr = []
 
-while mh.size > 1:
-    num = mh.remove()
-    sorted_arr.append(num)
+        arr = [2, 1, 5, 3, 4, 7, 6]
+        for i, n in enumerate(arr):
+            mh.heap_push(n)
 
-print(sorted_arr)
+        while mh.size > 1:
+            num = mh.heap_pop()
+            sorted_arr.append(num)
+
+        self.assertEquals(sorted(arr), sorted_arr)
