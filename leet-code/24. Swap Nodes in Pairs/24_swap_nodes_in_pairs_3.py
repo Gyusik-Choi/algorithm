472d2478804a359f5a8a0b737b27e56b29468a5f
kuidoli <kuidoli@hanmail.net>
2023-07-12 00:13:04 +0900

    leetcode 24

--- leet-code/24. Swap Nodes in Pairs/24_swap_nodes_in_pairs_3.py
+++ leet-code/24. Swap Nodes in Pairs/24_swap_nodes_in_pairs_3.py
@@ -10,9 +10,17 @@ def swap_pairs(head: ListNode):
     # if 문을 만족하지 않을때 두가지 경우가 있다
     # 1) head 가 None 이 아니고 head.next 가 None 이라면
     # head 가 가리키는 단일 노드가 리턴 된다
-    # p 에 head 의 next 가 가리키는 노드가 대입 되어 있고
-    # 앞서 리턴된 노드는 head 의 next 속성의 값으로 대입 된다
-    #
+    # 리턴된 값을 받는 호출 부분은
+    # head 의 다음 노드를 가리키던 p 와 head 가 바뀐다
+    # p -> head -> 리턴된 값 (단일 노드)
+    # 이런 형태로 값을 갖게 되고 여기서 p 를 다시 리턴 하고
+    # 이 값을 받는 부분의 p 와 head 도 바뀌고 끝에는
+    # p 가 리턴 되면서 함수가 종료된다
+    # 재귀의 끝에서 부터 뒤집힌 채로 값이 누적된 p 가 리턴된다
+    # 2) head 와 head.next 둘 다 None 이라면
+    # None 이 리턴 된다
+    # 리턴된 값을 받는 호출 부분은 1) 과 같이 p 와 head 가 바뀐다
+    # p -> head -> 리턴된 값 (None)
     if head and head.next:
         p = head.next
         head.next = swap_pairs(p.next)
