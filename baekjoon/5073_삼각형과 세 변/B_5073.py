1adb48049c71df3b6baa6dc4bfaf63c2fcef2c4b
kuidoli <kuidoli@hanmail.net>
2023-08-09 23:59:58 +0900

    Feat: baekjoon 5073

---
+++ baekjoon/5073_삼각형과 세 변/B_5073.py
@@ -0,0 +1,40 @@
+import sys
+
+
+def is_longer_than_max(a, b, c):
+    arr = [a, b, c]
+    max_num = max(arr)
+    arr.remove(max_num)
+    return max_num < sum(arr)
+
+
+def check_triangle():
+    answer = ''
+
+    while True:
+        a, b, c = map(int, sys.stdin.readline().split())
+
+        if a == b == c == 0:
+            break
+
+        if not is_longer_than_max(a, b, c):
+            answer += 'Invalid\n'
+            continue
+
+        if a == b == c:
+            answer += 'Equilateral\n'
+            continue
+
+        if len(list({a, b, c})) == 2:
+            answer += 'Isosceles\n'
+            continue
+
+        if a != b != c:
+            answer += 'Scalene\n'
+            continue
+
+    answer = answer.rstrip('\n')
+    sys.stdout.write(answer)
+
+
+check_triangle()
