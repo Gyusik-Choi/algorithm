b4115dfa77446248259807c5fca84abc08744b71
kuidoli <kuidoli@hanmail.net>
2023-08-17 23:13:29 +0900

    Feat: programmers 문자열 내 p와 y의 개수

---
+++ programmers/문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -0,0 +1,19 @@
+def solution(s):
+    s = s.lower()
+
+    p_count = 0
+    y_count = 0
+
+    for char in s:
+        if char == "p":
+            p_count += 1
+            continue
+
+        if char == "y":
+            y_count += 1
+
+    return p_count == y_count
+
+
+print(solution("pPoooyY"))
+print(solution("Pyy"))
