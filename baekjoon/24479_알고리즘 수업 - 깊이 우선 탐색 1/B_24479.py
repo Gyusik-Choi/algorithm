ab7cf566fcb90ec1fa5e0d2055c62cdbe8086154
최 규식 <kuidoli@hanmail.net>
2024-01-04 08:47:37 +0900

    Docs: update readme

--- baekjoon/24479_알고리즘 수업 - 깊이 우선 탐색 1/B_24479.py
+++ baekjoon/24479_알고리즘 수업 - 깊이 우선 탐색 1/B_24479.py
@@ -1,24 +1,24 @@
 from collections import defaultdict
 import sys
-sys.setrecursionlimit(10 ** 5)
+sys.setrecursionlimit(10 ** 6)
 
 
-def dfs(visit, edges, start):
+def dfs(visited, edges, start):
     cnt = 1
 
-    def dfs_recursion(v, e, r):
+    def dfs_recursion(visit, e, r):
         nonlocal cnt
 
-        v[r] = cnt
+        visit[r] = cnt
         cnt += 1
 
         for end in e[r]:
-            if not v[end]:
-                dfs_recursion(v, e, end)
+            if not visit[end]:
+                dfs_recursion(visit, e, end)
 
-        return v
+        return visit
 
-    return dfs_recursion(visit, edges, start)
+    return dfs_recursion(visited, edges, start)
 
 
 N, M, R = map(int, sys.stdin.readline().split())
@@ -31,9 +31,6 @@ for _ in range(M):
 for key in adj:
     adj[key].sort()
 
-if not len(adj[R]):
-    sys.stdout.write(str(0) + "\n")
-else:
-    visited = dfs([0] * (N + 1), adj, R)
-    for i in range(1, N + 1):
-        sys.stdout.write(str(visited[i]) + "\n")
+history = dfs([0] * (N + 1), adj, R)
+for i in range(1, N + 1):
+    sys.stdout.write(str(history[i]) + "\n")
