772599b05b0a34fb8a8287dda3ac453109a38bb9
최 규식 <kuidoli@hanmail.net>
2024-03-29 10:59:32 +0900

    Comment: modify comment of pivot

--- algorithm/sort/quick_sort_2.py
+++ algorithm/sort/quick_sort_2.py
@@ -22,9 +22,12 @@ def quick_sort(arr):
             if arr[right] < pivot:
                 arr[left], arr[right] = arr[right], arr[left]
                 left += 1
-        # pivot 이 아닌 arr[high] 로 한 이유는
-        # 위의 for 문에 의해 high 인덱스 값이
-        # pivot 이 아닌 다른 값으로 바뀔 수 있다
+        # pivot 은 리스트를 참조 하는게 아니라 정수 값을 갖는다
+        # arr[high] 가 아니라 pivot 을 쓴다면
+        # arr[left] 와 pivot 변수의 값이 바뀌는 것이다
+        # high 인덱스의 값에는 영향을 줄 수 없다
+        # high 인덱스의 값을 바꾸기 위해
+        # pivot 이 아니라 arr[high] 로 했다
         arr[left], arr[high] = arr[high], arr[left]
         return left
 
