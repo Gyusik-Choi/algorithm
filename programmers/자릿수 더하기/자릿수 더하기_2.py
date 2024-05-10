bc679c3c7ef60efa3d8a0fc78fac4561eafaa867
kuidoli <kuidoli@hanmail.net>
2024-05-10 10:11:41 +0900

    Feat: programmers 자릿수 더하기

---
+++ programmers/자릿수 더하기/자릿수 더하기_2.py
@@ -0,0 +1,6 @@
+def solution(n):
+    return sum(map(int, str(n)))
+
+
+print(solution(1234))
+print(solution(930211))
---
+++ programmers/자릿수 더하기/자릿수 더하기_3.py
@@ -0,0 +1,6 @@
+def solution(n):
+    return sum(int(i) for i in str(n))
+
+
+print(solution(1234))
+print(solution(930211))
