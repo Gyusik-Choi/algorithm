30ffd349601f5c2ad88633db9774a874c7c3b9ee
kuidoli <kuidoli@hanmail.net>
2024-07-30 08:11:10 +0900

    Feat: leet-code 561

---
+++ leet-code/561. Array Partition/ArrayPartition561.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArrayPartition561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sums = 0;
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            sums += nums[i];
+        }
+        return sums;
+    }
+}
