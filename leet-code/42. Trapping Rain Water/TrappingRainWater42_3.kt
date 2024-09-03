e4eda9c4d5bc39e83f430dfecc0e5cb607a362cb
kuidoli <kuidoli@hanmail.net>
2024-09-03 11:16:08 +0900

    Feat: leet-code 42

---
+++ leet-code/42. Trapping Rain Water/TrappingRainWater42_3.kt
@@ -0,0 +1,29 @@
+import kotlin.math.max
+
+class TrappingRainWater42_5 {
+    fun trap(height: IntArray): Int {
+        var sums: Int = 0
+        var left: Int = 0
+        var leftMax: Int = height[left]
+        var right: Int = height.size - 1
+        var rightMax: Int = height[right]
+
+        while (left < right) {
+            leftMax = max(leftMax, height[left])
+            rightMax = max(rightMax, height[right])
+
+            if (leftMax <= rightMax) {
+                if (leftMax > height[left]) {
+                    sums += leftMax - height[left]
+                }
+                left += 1
+            } else {
+                if (rightMax > height[right]) {
+                    sums += rightMax - height[right]
+                }
+                right -= 1
+            }
+        }
+        return sums
+    }
+}
