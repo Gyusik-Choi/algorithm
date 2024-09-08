f621c646770c83a7bd9b4f89d6f226c7b7e3375c
kuidoli <kuidoli@hanmail.net>
2024-09-08 21:52:50 +0900

    Feat: leet-code 706 - in progress

---
+++ leet-code/706. Design HashMap/(진행중)DesignHashMap706.java
@@ -0,0 +1,27 @@
+public class DesignHashMap706 {
+
+    // 0 ~ 10 ** 6 사이의 key 가 주어져서
+    // 이를 모듈로 연산으로 모두 담을 수 있는 1000001 크기의 배열을 생성
+    private Node[] nodes = new Node[1000001];
+
+    public void put(int key, int value) {
+
+    }
+
+    public int get(int key) {
+
+    }
+
+    public void remove(int key) {
+
+    }
+
+    private static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+}
