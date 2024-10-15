07f5ad600ff78bc8ada269f4389c014b7d4eb3a4
kuidoli <kuidoli@hanmail.net>
2024-10-15 10:20:58 +0900

    Feat: leet-code 687

---
+++ leet-code/687. Longest Univalue Path/LongestUnivaluePath687_2.java
@@ -0,0 +1,34 @@
+public class LongestUnivaluePath687_2 {
+    private int longestUnivalue = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        traverse(root);
+        return longestUnivalue;
+    }
+
+    public int traverse(TreeNode node) {
+        if (node == null) return 0;
+        int left = traverse(node.left);
+        int right = traverse(node.right);
+        return updateLongestUnivalue(node, left, right);
+    }
+
+    private int updateLongestUnivalue(TreeNode node, int left, int right) {
+        if (node.left != null && node.val == node.left.val && node.right != null && node.val == node.right.val) {
+            longestUnivalue = Math.max(longestUnivalue, left + right + 2);
+            return Math.max(left, right) + 1;
+        }
+
+        if (node.left != null && node.val == node.left.val) {
+            longestUnivalue = Math.max(longestUnivalue, left + 1);
+            return left + 1;
+        }
+
+        if (node.right != null && node.val == node.right.val) {
+            longestUnivalue = Math.max(longestUnivalue, right + 1);
+            return right + 1;
+        }
+
+        return 0;
+    }
+}
