9c1e98a5f41cdbcce4f395834ac6c4706f1a2bc7
kuidoli <kuidoli@hanmail.net>
2024-12-31 16:57:32 +0900

    Feat: leet-code 191

---
+++ leet-code/191. Number of 1 Bits/NumberOf1Bits191.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class NumberOf1Bits191 {
+    public int hammingWeight(int n) {
+        return Integer.bitCount(n);
+    }
+}
---
+++ leet-code/191. Number of 1 Bits/NumberOf1Bits191_2.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class NumberOf1Bits191_2 {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+}
