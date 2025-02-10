1485444953e080165848c5a37e150996ee452183
kuidoli <kuidoli@hanmail.net>
2025-02-10 09:35:22 +0900

    Feat: (in grogress of) programmers 92342

--- programmers/양궁대회/Programmers92342.java
+++ programmers/양궁대회/Programmers92342.java
@@ -1,42 +1,48 @@
 package com.example;
 
-public class Programmers92342_Fail {
+import java.util.Arrays;
 
-    private int lionNumMax;
+public class Programmers92342_2 {
 
-    private int[] lionNumMaxArr;
+    private int lionMax;
+
+    private int[] lionMaxArr;
 
     public int[] solution(int n, int[] info) {
-        lionNumMax = -1;
-        lionNumMaxArr = new int[info.length];
+        lionMax = -1;
+        lionMaxArr = new int[info.length];
         recursion(n, info, new int[info.length], 0);
-        if (lionNumMax == -1) {
-            return new int[]{-1};
-        }
-        return lionNumMaxArr;
+        return lionMax == -1 ? new int[]{-1} : lionMaxArr;
     }
 
-    private void recursion(int n, int[] apeach, int[] lion, int startIdx) {
-        if (n < 0) {
-            return;
-        }
-
-        if (n == 0) {
+    private void recursion(int n, int[] apeach, int[] lion, int idx) {
+        if (apeach.length - 1 == idx) {
+//            if (n > 0) {
+//                lion[idx] = n;
+//            }
+//            n 가 남았을 수 있다
+//            그러나 위와 같이
+//            n > 0 인 경우만 마지막 인덱스에 n 을 할당하면 안 된다
+//            재귀로 호출되기 때문에
+//            recursion(n - lion[idx], apeach, lion, idx + 1);
+//            여기서 n > 0 이면 마지막 인덱스에 n 을 할당하게 된다
+//            lion[idx] = 0; 이후에
+//            recursion(n, apeach, lion, idx + 1);
+//            여기서 n = 0 이면 마지막 인덱스에 n 을 할당하지 않으면서
+//            마지막 인덱스가 0으로 초기되지 못한다
+            lion[idx] = n;
+//             점수 비교
             compare(apeach, lion);
             return;
         }
 
-        for (int i = startIdx; i < apeach.length; i++) {
-            if (apeach[i] > 0) {
-                lion[i] += apeach[i] + 1;
-                recursion(n - lion[i], apeach, lion, i + 1);
-                lion[i] -= apeach[i] + 1;
-            } else {
-                lion[i] += 1;
-                recursion(n - 1, apeach, lion, i + 1);
-                lion[i] -= 1;
-            }
+        if (apeach[idx] < n) {
+            lion[idx] = apeach[idx] + 1;
+            recursion(n - lion[idx], apeach, lion, idx + 1);
+            lion[idx] = 0;
         }
+        recursion(n, apeach, lion, idx + 1);
+
     }
 
     private void compare(int[] apeach, int[] lion) {
@@ -57,21 +63,21 @@ public class Programmers92342_Fail {
             return;
         }
 
-        if (lionNum > lionNumMax) {
-            lionNumMax = lionNum;
-            System.arraycopy(lion, 0, lionNumMaxArr, 0, lion.length);
-        } else if (lionNum == lionNumMax) {
+        if (lionNum > lionMax) {
+            lionMax = lionNum;
+            System.arraycopy(lion, 0, lionMaxArr, 0, lion.length);
+        } else if (lionNum == lionMax) {
             if (canUpdateLionNumMaxArr(lion)) {
-                System.arraycopy(lion, 0, lionNumMaxArr, 0, lion.length);
+                System.arraycopy(lion, 0, lionMaxArr, 0, lion.length);
             }
         }
     }
 
     private boolean canUpdateLionNumMaxArr(int[] lion) {
         for (int i = lion.length - 1; i > -1; i--) {
-            if (lionNumMaxArr[i] > lion[i]) {
+            if (lionMaxArr[i] > lion[i]) {
                 return false;
-            } else if (lionNumMaxArr[i] < lion[i]) {
+            } else if (lionMaxArr[i] < lion[i]) {
                 return true;
             }
         }
