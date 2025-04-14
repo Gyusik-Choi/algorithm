f25e9264ec1e5fa8b007f5f466d2e4b46519fbe0
kuidoli <kuidoli@hanmail.net>
2025-04-14 10:50:01 +0900

    Chore: add comments for leet-code 23

--- leet-code/23. Merge K Sorted Lists/MergeKSortedLists23.java
+++ leet-code/23. Merge K Sorted Lists/MergeKSortedLists23.java
@@ -14,6 +14,11 @@ public class MergeKSortedLists23 {
         ListNode head = root;
         while (!pq.isEmpty()) {
             ListNode node = pq.poll();
+            // node 를 통채로 head.next 에 할당하긴 하지만
+            // 다음번 while 문에서 head.next 에 새로 node 를 할당한다
+            // 기존 node 의 길이가 2이상이라고 하더라도
+            // 기존 node 에서 맨 앞에 있는 노드를 제외하면
+            // 나머지 요소들은 다음번 while 문에서 덮어 써진다
             head.next = node;
             head = head.next;
             if (node.next != null) {
