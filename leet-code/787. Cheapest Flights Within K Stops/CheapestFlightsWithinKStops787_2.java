7c890091c312870fdcc723c9fadf5a1332d78807
kuidoli <kuidoli@hanmail.net>
2025-05-10 09:41:36 +0900

    Docs: add comments

--- leet-code/787. Cheapest Flights Within K Stops/CheapestFlightsWithinKStops787_2.java
+++ leet-code/787. Cheapest Flights Within K Stops/CheapestFlightsWithinKStops787_2.java
@@ -39,6 +39,11 @@ public class CheapestFlightsWithinKStops787_2 {
                 int price = node[1];
                 // 비용은 고려하지 않는다
                 if (stops[arrival] == 0 || (stops[arrival] > 0 && stops[arrival] > departure.stop + 1)) {
+                    // stops[arrival] = departure.stop + 1;
+                    // stops 를 여기서 갱신하면 안 된다
+                    // 쉽게 생각하면 보통 다익스트라를 구현할 때 여기서 방문 처리를 하지 않는다
+                    // 우선순위 큐에서 꺼내기도 전에 stops 가 갱신되어 버린다
+                    // 반례 -> (leetcode 예시 2) 3, [[0, 1, 100], [1, 2, 100], [0, 2, 500]], 0, 2, 1
                     pq.add(new Node(arrival, departure.price + price, departure.stop + 1));
                 }
             }
