d610423f160ff93e0e60fa4d8b545dbd1407b5db
kuidoli <kuidoli@hanmail.net>
2025-06-16 09:41:48 +0900

    Feat: add another merge sort method that doesn't sort parameter

--- algorithm/sort/MergeSort.kt
+++ algorithm/sort/MergeSort.kt
@@ -1,6 +1,9 @@
 package com.example.sort_kotlin
 
 class MergeSort {
+    /**
+     * 파라미터 arr 을 정렬
+     */
     fun sort(arr: IntArray) {
         mergeSort(arr, 0, arr.size - 1)
     }
@@ -39,5 +42,48 @@ class MergeSort {
         for (i in list.indices) {
             arr[i + low] = list[i]
         }
+    }
+
+    /**
+     * 파라미터 arr 을 정렬하지 않고 별도의 배열로 정렬해서 반환
+     */
+    fun sort2(arr: IntArray): IntArray {
+        val low = 0
+        val high = arr.size - 1
+
+        if (low >= high) {
+            return arr
+        }
+
+        val mid = low + (high - low) / 2
+        val lowArr = sort2(arr.slice(IntRange(low, mid)).toIntArray())
+        val highArr = sort2(arr.slice(IntRange(mid + 1, high)).toIntArray())
+
+        val list = IntArray(arr.size)
+        var idx = 0
+        var l = 0
+        var h = 0
+        while (l <= lowArr.size - 1 && h <= highArr.size - 1) {
+            if (lowArr[l] <= highArr[h]) {
+                list[idx] = lowArr[l]
+                l += 1
+            } else {
+                list[idx] = highArr[h]
+                h += 1
+            }
+            idx += 1
+        }
+        while (l <= lowArr.size - 1) {
+            list[idx] = lowArr[l]
+            l += 1
+            idx += 1
+        }
+        while (h <= highArr.size - 1) {
+            list[idx] = highArr[h]
+            h += 1
+            idx += 1
+        }
+
+        return list
     }
 }
