caa415d4b908f574ff1820d2463e740eac4982ba
kuidoli <kuidoli@hanmail.net>
2025-07-11 10:20:46 +0900

    Style: inline Arrays.stream

--- leet-code/136. Single Number/SingleNumber136_2.java
+++ leet-code/136. Single Number/SingleNumber136_2.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 
 public class SingleNumber136_2 {
     public int singleNumber(int[] nums) {
-        return Arrays
-                .stream(nums)
+        return Arrays.stream(nums)
                 .reduce((x, y) -> x ^ y)
                 .getAsInt();
     }
