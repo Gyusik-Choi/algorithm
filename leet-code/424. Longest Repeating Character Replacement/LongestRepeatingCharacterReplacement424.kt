b8693593f1c047e0e0b54847f90547bc64cd29c7
kuidoli <kuidoli@hanmail.net>
2025-07-23 10:35:24 +0900

    Feat: leet-code 424 kotlin - add another solution

--- leet-code/424. Longest Repeating Character Replacement/LongestRepeatingCharacterReplacement424.kt
+++ leet-code/424. Longest Repeating Character Replacement/LongestRepeatingCharacterReplacement424.kt
@@ -20,4 +20,21 @@ class LongestRepeatingCharacterReplacement424 {
         }
         return answer
     }
+
+//    아래의 풀이도 가능하다
+//    (교재는 아래처럼 정답 변수를 따로 두지 않으면서 마지막에 한번만 구하는 방식을 사용했다)
+//    fun characterReplacement(s: String, k: Int): Int {
+//        val count = mutableMapOf<Char, Int>()
+//        var left = 0
+//        for (right in s.indices) {
+//            count[s[right]] = count.getOrDefault(s[right], 0) + 1
+//            // 해시맵에서 가장 큰 갯수 + k >= 윈도우 크기
+//            val maxEntry = count.maxBy { it.value }
+//            if (maxEntry.value + k < right - left + 1) {
+//                count[s[left]] = count.getOrDefault(s[left], 0) - 1
+//                left += 1
+//            }
+//        }
+//        return s.length - left
+//    }
 }
