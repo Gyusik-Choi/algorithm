cf943084ba33e934cbfb36d509fae8a2e04cd0c1
kuidoli <kuidoli@hanmail.net>
2025-07-29 11:03:24 +0900

    Chore: remove wrong comments

--- leet-code/455. Assign Cookies/AssignCookies455.kt
+++ leet-code/455. Assign Cookies/AssignCookies455.kt
@@ -2,12 +2,6 @@ package com.example
 
 class AssignCookies455 {
     fun findContentChildren(g: IntArray, s: IntArray): Int {
-        // g 와 s 를 정렬한다
-        // s 의 for 문을 돌면서
-        // g 에 만족하는 요소를 찾는다
-        // 만약에 현재 s 가 g 에 만족하는 요소를 못 찾으면
-        // 더 이상 만족하는 요소를 찾을 수 없기 때문에
-        // s 의 for 문을 종료한다
         g.sort()
         s.sort()
         var maxNumber = 0
