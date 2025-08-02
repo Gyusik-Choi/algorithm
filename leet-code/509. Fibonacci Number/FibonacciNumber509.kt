1da6572273faa7cf4940595b66127fa407ec4c01
kuidoli <kuidoli@hanmail.net>
2025-08-02 10:09:02 +0900

    Feat: leet-code 509 kotlin

---
+++ leet-code/509. Fibonacci Number/FibonacciNumber509.kt
@@ -0,0 +1,10 @@
+package com.example
+
+class FibonacciNumber509 {
+    fun fib(n: Int): Int {
+        if (n < 2) {
+            return n
+        }
+        return fib(n - 1) + fib(n - 2)
+    }
+}
