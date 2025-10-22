69c1ec65f7de65700f21913611f91d39f09829f0
최 규식 <kuidoli@hanmail.net>
2025-10-23 08:43:05 +0900

    Feat: leet-code 226 java

---
+++ leet-code/226. Invert Binary Tree/InvertBinaryTree226_5.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class InvertBinaryTree226_5 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
