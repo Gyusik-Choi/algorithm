13c46529700fafa56a0e34cc64b0288b99b44c36
최 규식 <kuidoli@hanmail.net>
2025-12-17 08:45:58 +0900

    Feat: leet-code 76 java

--- leet-code/76. Minimum Window Substring/MinimumWindowSubstring76_2.java
+++ leet-code/76. Minimum Window Substring/MinimumWindowSubstring76_2.java
@@ -9,14 +9,6 @@ public class MinimumWindowSubstring76_2 {
         if (s.length() < t.length()) {
             return "";
         }
-        if (s.length() == 1 && t.length() == 1 && !s.equals(t)) {
-            return "";
-        }
-        if (s.equals(t)) {
-            return s;
-        }
-        // ZDBWETASETEAB
-        // ABA
         // 오른쪽으로 이동하고
         // 왼쪽도 이동할 수 있으면 최대한 이동한다
         // 왼쪽을 이동하려면
@@ -26,25 +18,28 @@ public class MinimumWindowSubstring76_2 {
         for (char tChar : t.toCharArray()) {
             tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
         }
-        int start = 0, end = s.length() - 1;
         boolean isModified = false;
+        int start = 0, end = s.length() - 1;
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            if (!tMap.containsKey(c)) {
+            char sCharRight = s.charAt(right);
+            if (!tMap.containsKey(sCharRight)) {
                 continue;
             }
-            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+            sMap.put(sCharRight, sMap.getOrDefault(sCharRight, 0) + 1);
             if (containsAll(sMap, tMap)) {
-                while (left < right && (!sMap.containsKey(s.charAt(left)) || sMap.get(s.charAt(left)) > tMap.get(s.charAt(left)))) {
-                    if (!sMap.containsKey(s.charAt(left))) {
+                while (left < right) {
+                    char sCharLeft = s.charAt(left);
+                    if (!sMap.containsKey(sCharLeft)) {
                         left += 1;
                         continue;
                     }
-                    if (sMap.get(s.charAt(left)) > tMap.get(s.charAt(left))) {
-                        sMap.put(s.charAt(left), sMap.get(s.charAt(left)) - 1);
+                    if (sMap.get(sCharLeft) > tMap.get(sCharLeft)) {
+                        sMap.put(sCharLeft, sMap.get(sCharLeft) - 1);
                         left += 1;
+                        continue;
                     }
+                    break;
                 }
                 if (end - start >= right - left) {
                     start = left;
@@ -53,10 +48,7 @@ public class MinimumWindowSubstring76_2 {
                 }
             }
         }
-        if (!isModified) {
-            return "";
-        }
-        return s.substring(start, end + 1);
+        return isModified ? s.substring(start, end + 1) : "";
     }
 
     private boolean containsAll(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
@@ -75,4 +67,136 @@ public class MinimumWindowSubstring76_2 {
         }
         return true;
     }
+
+//    통과 코드 2)
+//    public String minWindow(String s, String t) {
+//        if (s.length() < t.length()) {
+//            return "";
+//        }
+//        if (s.equals(t)) {
+//            return s;
+//        }
+//        Map<Character, Integer> sMap = new HashMap<>();
+//        Map<Character, Integer> tMap = new HashMap<>();
+//        for (char tChar : t.toCharArray()) {
+//            tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
+//        }
+//        boolean isModified = false;
+//        int start = 0, end = s.length() - 1;
+//        int left = 0;
+//        for (int right = 0; right < s.length(); right++) {
+//            char c = s.charAt(right);
+//            if (!tMap.containsKey(c)) {
+//                continue;
+//            }
+//            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+//            if (containsAll(sMap, tMap)) {
+//                while (left < right) {
+//                    char sCharLeft = s.charAt(left);
+//                    if (!sMap.containsKey(sCharLeft)) {
+//                        left += 1;
+//                        continue;
+//                    }
+//                    if (sMap.get(sCharLeft) > tMap.get(sCharLeft)) {
+//                        sMap.put(sCharLeft, sMap.get(sCharLeft) - 1);
+//                        left += 1;
+//                        continue;
+//                    }
+//                    break;
+//                }
+//                if (end - start >= right - left) {
+//                    start = left;
+//                    end = right;
+//                    isModified = true;
+//                }
+//            }
+//        }
+//        if (!isModified) {
+//            return "";
+//        }
+//        return s.substring(start, end + 1);
+//    }
+//
+//    private boolean containsAll(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
+//        Set<Character> sKeys = sMap.keySet();
+//        if (sKeys.isEmpty()) {
+//            return false;
+//        }
+//        Set<Character> tKeys = tMap.keySet();
+//        for (Character key : tKeys) {
+//            if (!sMap.containsKey(key)) {
+//                return false;
+//            }
+//            if (sMap.get(key) < tMap.get(key)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+//    통과 코드 1)
+//    public String minWindow(String s, String t) {
+//        if (s.length() < t.length()) {
+//            return "";
+//        }
+//        if (s.length() == 1 && t.length() == 1 && !s.equals(t)) {
+//            return "";
+//        }
+//        if (s.equals(t)) {
+//            return s;
+//        }
+//        Map<Character, Integer> sMap = new HashMap<>();
+//        Map<Character, Integer> tMap = new HashMap<>();
+//        for (char tChar : t.toCharArray()) {
+//            tMap.put(tChar, tMap.getOrDefault(tChar, 0) + 1);
+//        }
+//        int start = 0, end = s.length() - 1;
+//        boolean isModified = false;
+//        int left = 0;
+//        for (int right = 0; right < s.length(); right++) {
+//            char c = s.charAt(right);
+//            if (!tMap.containsKey(c)) {
+//                continue;
+//            }
+//            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+//            if (containsAll(sMap, tMap)) {
+//                while (left < right && (!sMap.containsKey(s.charAt(left)) || sMap.get(s.charAt(left)) > tMap.get(s.charAt(left)))) {
+//                    if (!sMap.containsKey(s.charAt(left))) {
+//                        left += 1;
+//                        continue;
+//                    }
+//                    if (sMap.get(s.charAt(left)) > tMap.get(s.charAt(left))) {
+//                        sMap.put(s.charAt(left), sMap.get(s.charAt(left)) - 1);
+//                        left += 1;
+//                    }
+//                }
+//                if (end - start >= right - left) {
+//                    start = left;
+//                    end = right;
+//                    isModified = true;
+//                }
+//            }
+//        }
+//        if (!isModified) {
+//            return "";
+//        }
+//        return s.substring(start, end + 1);
+//    }
+//
+//    private boolean containsAll(Map<Character, Integer> sMap, Map<Character, Integer> tMap) {
+//        Set<Character> sKeys = sMap.keySet();
+//        if (sKeys.isEmpty()) {
+//            return false;
+//        }
+//        Set<Character> tKeys = tMap.keySet();
+//        for (Character key : tKeys) {
+//            if (!sMap.containsKey(key)) {
+//                return false;
+//            }
+//            if (sMap.get(key) < tMap.get(key)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 }
