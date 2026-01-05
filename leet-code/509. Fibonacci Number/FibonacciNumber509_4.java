da93a815a06543c54e7239e4a26410a81bbac2e4
최 규식 <kuidoli@hanmail.net>
2026-01-05 09:45:56 +0900

    Feat: leet-code 509 java

---
+++ leet-code/509. Fibonacci Number/FibonacciNumber509_4.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class FibonacciNumber509_4 {
+    private final int[] arr = new int[31];
+
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (arr[n] > 0) {
+            return arr[n];
+        }
+        arr[n] = fib(n - 1) + fib(n - 2);
+        return arr[n];
+    }
+}
