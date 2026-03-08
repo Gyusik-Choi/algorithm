281b427408b65635e6e42dcde7c9a6d0c20865ef
kuidoli <kuidoli@hanmail.net>
2026-03-08 10:40:37 +0900

    Feat: bubble sort

---
+++ algorithm/sort/bubble_sort.c
@@ -0,0 +1,11 @@
+void bubble_sort(int *arr, const int size) {
+    for (int i = size - 1; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
