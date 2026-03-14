0249fbeb2b022fcd04b995836bb822cd83231c83
최 규식 <kuidoli@hanmail.net>
2026-03-14 10:34:21 +0900

    Feat: leet-code 622 java

---
+++ leet-code/622. Design Cirular Queue/DesignCircularQueue622_6.java
@@ -0,0 +1,70 @@
+package com.example;
+
+public class DesignCircularQueue622_6 {
+    private Node front = null;
+    private Node rear = null;
+    private int curSize = 0;
+    private final int maxSize;
+
+    public DesignCircularQueue622_6(int k) {
+        maxSize = k;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            front = new Node(value);
+            rear = front;
+        } else {
+            Node oldRear = rear;
+            rear = new Node(value);
+            oldRear.next = rear;
+        }
+        curSize += 1;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        // front 와 rear.next 의 참조를 변경해야 한다
+        if (curSize == 1) {
+            front = null;
+            rear = null;
+        } else {
+            Node next = front.next;
+            front = front.next;
+            rear.next = next;
+        }
+        curSize -= 1;
+        return true;
+    }
+
+    public int Front() {
+        return isEmpty() ? -1 : front.value;
+    }
+
+    public int Rear() {
+        return isEmpty() ? -1 : rear.value;
+    }
+
+    public boolean isEmpty() {
+        return curSize == 0;
+    }
+
+    public boolean isFull() {
+        return curSize == maxSize;
+    }
+
+    private class Node {
+        final int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+}
