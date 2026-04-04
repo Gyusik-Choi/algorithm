157cce1a252ef173a5225f7454135a3e3e117bbb
최 규식 <kuidoli@hanmail.net>
2026-04-04 10:27:35 +0900

    Feat: leet-code 200 c

---
+++ leet-code/200. Number of Islands/200_number_of_islands.c
@@ -0,0 +1,24 @@
+static void dfs_recursion(char** grid, const int gridSize, int* gridColSize, const int i, const int j) {
+    if (0 > i || i >= gridSize || 0 > j || j >= gridColSize[i] || grid[i][j] != '1') {
+        return;
+    }
+    grid[i][j] = '0';
+    dfs_recursion(grid, gridSize, gridColSize, i - 1, j);
+    dfs_recursion(grid, gridSize, gridColSize, i, j + 1);
+    dfs_recursion(grid, gridSize, gridColSize, i + 1, j);
+    dfs_recursion(grid, gridSize, gridColSize, i, j - 1);
+}
+
+int numIslands(char** grid, int gridSize, int* gridColSize)
+{
+    int cnt = 0;
+    for (int i = 0; i < gridSize; i++) {
+        for (int j = 0; j < gridColSize[i]; j++) {
+            if (grid[i][j] == '1') {
+                cnt += 1;
+                dfs_recursion(grid, gridSize, gridColSize, i, j);
+            }
+        }
+    }
+    return cnt;
+}
