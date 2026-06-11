ad6349edec897e9cbc9ecbbc24a12f0efb05e743
최 규식 <kuidoli@hanmail.net>
2026-06-12 08:40:45 +0900

    Fix: manage negative integer value in count sort

--- algorithm/sort/CountingSort2.java
+++ algorithm/sort/CountingSort2.java
@@ -3,19 +3,29 @@ package com.example;
 public class CountingSort2 {
     public int[] sort(int[] arr) {
         int maxValue = getMaxValue(arr);
-        int[] countArr = new int[maxValue + 1];
+        // 음수 값에 대비하여 offset 만큼 인덱스를 양수로 이동한다
+        int offset = getMinValue(arr);
+        int[] countArr = new int[maxValue - offset + 1];
         for (int i = 0; i < arr.length; i++) {
-            countArr[arr[i]] += 1;
+            countArr[arr[i] - offset] += 1;
         }
         for (int i = 1; i < countArr.length; i++) {
             countArr[i] += countArr[i - 1];
         }
         int[] answer = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            countArr[arr[i]] -= 1;
-            answer[countArr[arr[i]]] = arr[i];
+            countArr[arr[i] - offset] -= 1;
+            answer[countArr[arr[i] - offset]] = arr[i];
         }
         return answer;
+    }
+
+    private int getMinValue(int[] arr) {
+        int minValue = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            minValue = Math.min(minValue, arr[i]);
+        }
+        return minValue;
     }
 
     private int getMaxValue(int[] arr) {
