80fe87b4e40f582d4db25f4d232f361aee74bfe0
최 규식 <kuidoli@hanmail.net>
2026-06-29 09:05:16 +0900

    Feat: leet-code 704 java

---
+++ leet-code/704. Binary Search/BinarySearch704_3.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class BinarySearch704_3 {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low] == target ? low : -1;
+        // [1, 2, 3, 4, 5, 5, 5] / 5
+    }
+}
