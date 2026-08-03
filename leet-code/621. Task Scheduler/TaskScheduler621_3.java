ef4f4c49a23f0e9dabe553b56bb6bc14b34ab95a
kuidoli <kuidoli@hanmail.net>
2026-08-04 08:47:35 +0900

    Refactor: leet-code 621 java

--- leet-code/621. Task Scheduler/TaskScheduler621_3.java
+++ leet-code/621. Task Scheduler/TaskScheduler621_3.java
@@ -19,40 +19,40 @@ public class TaskScheduler621_3 {
         while (!pq.isEmpty()) {
             Node node = pq.poll();
             if (canAdd(window, node.character)) {
-                count += 1;
                 node.minusCount();
                 if (node.count > 0) {
                     pq.add(node);
                 }
-                list.add(node);
-                window.put(node.character, window.getOrDefault(node.character, 0) + 1);
-                pq.addAll(nodeList);
-                nodeList.clear();
-                if (list.size() > n) {
-                    Node pop = list.removeFirst();
-                    window.put(pop.character, window.get(pop.character) - 1);
-                }
             } else {
                 nodeList.add(node);
-                if (pq.isEmpty()) {
-                    // idle
-                    count += 1;
-                    list.add(new Node('i', 1));
-                    window.put('i', window.getOrDefault('i', 0) + 1);
-                    if (list.size() > n) {
-                        Node pop = list.removeFirst();
-                        window.put(pop.character, window.get(pop.character) - 1);
-                    }
-                    pq.addAll(nodeList);
-                    nodeList.clear();
+                if (!pq.isEmpty()) {
+                    continue;
                 }
+                node = new Node('i', 1);
             }
+            count += 1;
+            list.add(node);
+            window.put(node.character, window.getOrDefault(node.character, 0) + 1);
+            clearNodeList(pq, nodeList);
+            checkWindow(list, window, n);
         }
         return count;
     }
 
     private boolean canAdd(Map<Character, Integer> map, char c) {
         return !map.containsKey(c) || map.get(c) <= 0;
+    }
+
+    private void clearNodeList(PriorityQueue<Node> pq, List<Node> nodeList) {
+        pq.addAll(nodeList);
+        nodeList.clear();
+    }
+
+    private void checkWindow(List<Node> list, Map<Character, Integer> window, int n) {
+        if (list.size() > n) {
+            Node pop = list.removeFirst();
+            window.put(pop.character, window.get(pop.character) - 1);
+        }
     }
 
     static class Node {
@@ -77,44 +77,54 @@ public class TaskScheduler621_3 {
 //public class TaskScheduler621_3 {
 //    public int leastInterval(char[] tasks, int n) {
 //        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> b.count - a.count);
-//        HashMap<Character, Integer> map = new HashMap<>();
+//        Map<Character, Integer> map = new HashMap<>();
 //        for (char t : tasks) {
 //            map.put(t, map.getOrDefault(t, 0) + 1);
 //        }
 //        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
 //            pq.add(new Node(entry.getKey(), entry.getValue()));
 //        }
-//        List<Character> answer = new ArrayList<>();
+//        int count = 0;
+//        Map<Character, Integer> window = new HashMap<>();
+//        List<Node> list = new ArrayList<>();
 //        List<Node> nodeList = new ArrayList<>();
 //        while (!pq.isEmpty()) {
 //            Node node = pq.poll();
-//            if (canAdd(answer, node.character, Math.max(answer.size() - n, 0), answer.size() - 1)) {
-//                answer.add(node.character);
+//            if (canAdd(window, node.character)) {
+//                count += 1;
 //                node.minusCount();
 //                if (node.count > 0) {
 //                    pq.add(node);
 //                }
+//                list.add(node);
+//                window.put(node.character, window.getOrDefault(node.character, 0) + 1);
 //                pq.addAll(nodeList);
 //                nodeList.clear();
+//                if (list.size() > n) {
+//                    Node pop = list.removeFirst();
+//                    window.put(pop.character, window.get(pop.character) - 1);
+//                }
 //            } else {
 //                nodeList.add(node);
 //                if (pq.isEmpty()) {
-//                    answer.add('i');
+//                    // idle
+//                    count += 1;
+//                    list.add(new Node('i', 1));
+//                    window.put('i', window.getOrDefault('i', 0) + 1);
+//                    if (list.size() > n) {
+//                        Node pop = list.removeFirst();
+//                        window.put(pop.character, window.get(pop.character) - 1);
+//                    }
 //                    pq.addAll(nodeList);
 //                    nodeList.clear();
 //                }
 //            }
 //        }
-//        return answer.size();
+//        return count;
 //    }
 //
-//    private boolean canAdd(List<Character> list, char ch, int start, int end) {
-//        for (int i = start; i <= end; i++) {
-//            if (list.get(i).equals(ch)) {
-//                return false;
-//            }
-//        }
-//        return true;
+//    private boolean canAdd(Map<Character, Integer> map, char c) {
+//        return !map.containsKey(c) || map.get(c) <= 0;
 //    }
 //
 //    static class Node {
