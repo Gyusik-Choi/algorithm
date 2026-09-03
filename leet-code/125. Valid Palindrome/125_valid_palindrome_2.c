9ee4fc15957f45d7eee7b6f294bff73530fa4ac2
kuidoli <kuidoli@hanmail.net>
2026-09-04 08:45:16 +0900

    Feat: leet-code 125 c

---
+++ leet-code/125. Valid Palindrome/125_valid_palindrome_2.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+bool isPalindrome(char* s) {
+    int left = 0, right = strlen(s);
+    while (left < right) {
+        while (left < right && !isalnum(s[left])) {
+            left++;
+        }
+        while (left < right && !isalnum(s[right])) {
+            right--;
+        }
+        if (tolower(s[left]) != tolower(s[right])) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
